d96e282ae177a7effb8a9073312a8f765cf19543
chrispham <66860671+chrispham45@users.noreply.github.com>
2020-08-26 00:16:47 +1200

    Bug fix, custom vc tracker
    - Fix the bug when one person left the vc, the bot deletes the vc.
    - Add a custom voice channels tracker

--- chrispbirb.py
+++ chrispbirb.py
@@ -11,16 +11,20 @@ token = env.get("TOKEN")
 client = commands.Bot(command_prefix='>')
 
 
+# keep track of the custom voice sessions
+custom_voice_channels = {}
+
+
 # Custom help message - to be done
 client.remove_command('help')
 @client.command()
 async def help(ctx):
-    await ctx.send("```This is the help message\n ```")
+    await ctx.send("`Hop in the \"Create New Session\" voice channel to generate a voice chat for your use`")
 
 
 @client.event
 async def on_ready():
-    print('Chrispbirb is now deployed.')
+    print('Chrispbirb has been deployed.')
 
 
 @client.event
@@ -38,12 +42,27 @@ async def on_voice_state_update(member, before, after):
 
             # Create the new session on the same category as the initialize voice channel
             new_session = await member.guild.categories[category_index].create_voice_channel(channel_name)
-            await member.move_to(new_session)
 
-    # Remove the session as the user exit
+            # Add the voice channel instance to the tracker
+            custom_voice_channels[new_session.name] = -1 # -1 as the user join counts twice first time
+            await member.move_to(new_session)
+    
+    
+    #update the counter everytime someone enters a custom session
+    if after.channel is not None:
+        if after.channel.name in custom_voice_channels:
+            custom_voice_channels[after.channel.name] += 1
+
+
+    # Remove the session as the all user exit - TO FIX THE COUNTER DOESN'T UPDATE THE FIRST TIME AN USER EXITS (doesn't affect functionality)
     if before.channel is not None:
-        if before.channel.name[-10:] == "'s session":
-            await before.channel.delete()
+        if before.channel.name in custom_voice_channels:
+            user_count = custom_voice_channels[before.channel.name]
+            if before.channel.name in custom_voice_channels and user_count > 0:
+                custom_voice_channels[before.channel.name] -= 1
+            if before.channel.name in custom_voice_channels and user_count == 1:
+                time.sleep(1)
+                await before.channel.delete()
 
 
 @client.command()
